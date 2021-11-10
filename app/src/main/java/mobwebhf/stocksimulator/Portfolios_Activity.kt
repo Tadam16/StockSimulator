318efa23c58c233a68cd67a21894969528b442cf
me <example@mail.com>
2021-11-10 23:29:53 +0100

    portfolios design

--- app/src/main/java/mobwebhf/stocksimulator/Portfolios_Activity.kt
+++ app/src/main/java/mobwebhf/stocksimulator/Portfolios_Activity.kt
@@ -3,9 +3,9 @@ package mobwebhf.stocksimulator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class Portfolios_Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.portfolios)
     }
 }
