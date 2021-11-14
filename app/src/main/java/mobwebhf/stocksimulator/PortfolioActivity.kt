97ce3bd98d538ca5bb4d165600aa5eeb5daa5391
tadam <example@example.com>
2021-11-14 02:16:52 +0100

    Classes

--- app/src/main/java/mobwebhf/stocksimulator/PortfolioActivity.kt
+++ app/src/main/java/mobwebhf/stocksimulator/PortfolioActivity.kt
@@ -3,7 +3,7 @@ package mobwebhf.stocksimulator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class Portfolios_Activity : AppCompatActivity() {
+class PortfolioActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.portfolios)
---
+++ app/src/main/java/mobwebhf/stocksimulator/PortfolioDialog.kt
@@ -0,0 +1,4 @@
+package mobwebhf.stocksimulator
+
+class PortfolioDialog {
+}
