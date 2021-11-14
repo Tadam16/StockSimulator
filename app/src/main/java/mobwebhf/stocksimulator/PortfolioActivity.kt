fc50017b5a4b2b25410e43c49070446baefdaa35
tadam <example@example.com>
2021-11-14 03:22:33 +0100

    portfolios view

--- app/src/main/java/mobwebhf/stocksimulator/PortfolioActivity.kt
+++ app/src/main/java/mobwebhf/stocksimulator/PortfolioActivity.kt
@@ -1,11 +1,23 @@
 package mobwebhf.stocksimulator
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import androidx.appcompat.app.AppCompatActivity
+import mobwebhf.stocksimulator.databinding.PortfoliosBinding
 
 class PortfolioActivity : AppCompatActivity() {
+
+    private lateinit var binding : PortfoliosBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.portfolios)
+
+        binding = PortfoliosBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.portfolio_toolbar_menu, menu)
+        return true;
     }
 }
